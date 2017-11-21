de4a0dd5ce5c62ec76c51d3dfc2d444a9dc692d3
Kerem Yardım <kerem.yardim@lunatr.com>
2017-11-21 10:10:08 +0300

    Update BasaeContract comment

--- Vehicle/Vehicle.Business/Utils/IBaseContract.cs
+++ Vehicle/Vehicle.Business/Utils/IBaseContract.cs
@@ -11,13 +11,55 @@ namespace Vehicle.Business.Utils
         where E : class
         where Key : struct
     {
+        /// <summary>
+        /// Add New Record to DB. Using parameter EditViewModel
+        /// </summary>
+        /// <param name="NewRecord"></param>
+        /// <returns></returns>
         Task AddRecord(E NewRecord);
+        /// <summary>
+        /// Update current record. Using parameter EditViewModel
+        /// </summary>
+        /// <param name="UpdateRecord"></param>
+        /// <returns></returns>
         Task UpdateRecord(E UpdateRecord);
+        /// <summary>
+        /// Delete Current Record. 
+        /// </summary>
+        /// <param name="RecordId"></param>
+        /// <returns></returns>
         Task DeleteRecord(Key RecordId);
+        /// <summary>
+        /// Get spesific data whit key
+        /// </summary>
+        /// <param name="RecordId"></param>
+        /// <returns></returns>
         Task<T> GetRecordInfo(Key RecordId);
+        /// <summary>
+        /// Get spesific data for editing
+        /// </summary>
+        /// <param name="RecordId"></param>
+        /// <returns></returns>
         Task<E> GetEditRecordInfo(Key RecordId);
+        /// <summary>
+        /// Get result set from db with parameter
+        /// </summary>
+        /// <param name="SortOrder"></param>
+        /// <param name="CurrentFilter"></param>
+        /// <param name="SearchString"></param>
+        /// <param name="Page"></param>
+        /// <returns></returns>
         Task<PaginatedList<T>> GetRecords(string SortOrder, string CurrentFilter, string SearchString, int? Page);
+        /// <summary>
+        /// Get all record from db
+        /// </summary>
+        /// <returns></returns>
         Task<List<T>> GetRecords();
+        /// <summary>
+        /// Get all active record from db to select component data
+        /// </summary>
+        /// <param name="SelectedID"></param>
+        /// <returns></returns>
         Task<List<SelectListItem>> GetRecordsForDropdown(int? SelectedID = null);
 
     }
